73f110ae00b1714a15603882c08a17c51b3e9f93
OmarKh22 <ok597451@gmail.com>
2023-08-12 16:11:26 +0300

    change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,15 +15,15 @@ const Navbar = () => (
     <div
       className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
     >
-      <img
-      src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className='text-white font-extrabold text-[24px] leading-[30px]'>
+    <img
+    src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain"
+    />
+      <h2 className="text-white font-extrabold text-[24px] leading-[30px]">
         METAVERSUS
       </h2>
-      <img
-      src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"
-      />
+    <img
+    src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"
+    />
     </div>
   </motion.nav>
 );
